ba0bc9b4b0cd3f52426f51e3d5207e52f07797a7
宋文田 <songwentian@126.com>
2020-07-17 09:12:14 +0800

    抓取列表+详情页

--- jianshu/spiders/main.py
+++
@@ -1,9 +0,0 @@
-# _*_ coding: utf-8 _*_
-
-from scrapy.cmdline import execute
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath('D:\jianshu\jianshu\spiders\jianshuSpider.py'))) #设置工程目录
-print(os.path.dirname(os.path.abspath('D:\jianshu\jianshu\spiders\jianshuSpider.py')))
-
-execute(["scrapy","crawl","movie"]).strip()
